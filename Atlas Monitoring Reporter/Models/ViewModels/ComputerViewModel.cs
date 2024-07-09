cb7cfe060851711cf747c9c2cee5a1d62f463fd7
Paulin <buland2001@gmail.com>
2024-07-09 22:01:01 +0200

    Service link to API

---
+++ Atlas Monitoring Reporter/Models/ViewModels/ComputerViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Atlas_Monitoring_Reporter.Models.Database;
+using Atlas_Monitoring_Reporter.Models.Internal;
+
+namespace Atlas_Monitoring_Reporter.Models.ViewModels
+{
+    public class ComputerReadViewModel
+    {
+        public Guid Id { get; set; }
+        public DeviceStatus DeviceStatus { get; set; } = DeviceStatus.New;
+        public DeviceType DeviceType { get; set; } = DeviceType.Computer;
+        public string Name { get; set; } = string.Empty;
+        public string Ip { get; set; } = string.Empty;
+        public string Domain { get; set; } = string.Empty;
+        public double MaxRam { get; set; } = 0;
+        public double NumberOfLogicalProcessors { get; set; } = 0;
+        public string OS { get; set; } = string.Empty;
+        public string OSVersion { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string SerialNumber { get; set; } = string.Empty;
+        public DateTime DateAdd { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public List<ComputerHardDriveViewModel> ComputerHardDrives = new();
+        public List<ComputerDataViewModel> ComputerLastData = new();
+    }
+
+    public class ComputerWriteViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Ip { get; set; } = string.Empty;
+        public string Domain { get; set; } = string.Empty;
+        public double MaxRam { get; set; } = 0;
+        public double NumberOfLogicalProcessors { get; set; } = 0;
+        public string OS { get; set; } = string.Empty;
+        public string OSVersion { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string SerialNumber { get; set; } = string.Empty;
+        public DateTime DateAdd { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        public List<ComputerHardDriveViewModel> ComputerHardDrives = new();
+        public ComputerDataViewModel ComputerLastData = new();
+    }
+}
--- Atlas Monitoring Reporter/Worker.cs
+++ Atlas Monitoring Reporter/Worker.cs
@@ -1,23 +1,241 @@
+using Atlas_Monitoring.Core.Models.Database;
+using Atlas_Monitoring_Reporter.Models.ViewModels;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Management;
+using System.Net;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+
 namespace Atlas_Monitoring_Reporter
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly string _apiPath = string.Empty;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            _apiPath = "http://localhost:8080/api";
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                if (_logger.IsEnabled(LogLevel.Information))
+                try
                 {
-                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    //Step 1 : Get Data
+                    ComputerWriteViewModel computerWriteViewModel = GetStaticInformationOfComputer();
+
+                    Guid computerId = await CheckIfComputerExist(computerWriteViewModel.Name, computerWriteViewModel.SerialNumber);
+
+                    //Step 2 : If the computer don't exist, create it 
+                    if (computerId == Guid.Empty)
+                    {
+                        computerId = await CreateComputerInDataBase(computerWriteViewModel);
+                    }
+                    //Step 2.1 : If the computer exist, save the id of the computer and update information (Ip, domain, user, etc...)
+                    else
+                    {
+                        computerWriteViewModel.Id = computerId;
+                        computerId = await UpdateComputerInDataBase(computerWriteViewModel);
+                    }
+
+                    //Step 3 : Send Computer Data
+                    ComputerDataViewModel computerDataViewModel = computerWriteViewModel.ComputerLastData;
+                    computerDataViewModel.ComputerId = computerId;
+
+                    await AddComputerData(computerDataViewModel);
+
+                    //Step 4 : Update Hard drive
+                    foreach (ComputerHardDriveViewModel computerHardDriveViewModel in computerWriteViewModel.ComputerHardDrives)
+                    {
+                        computerHardDriveViewModel.ComputerId = computerId;
+
+                        await AddComputerHardDrive(computerHardDriveViewModel);
+                    }
+
+                    //Step 5 : Delay between two report
+                    int delay = 1000 * 60 * 5; //5 minutes delay
+                    await Task.Delay(delay, stoppingToken);
                 }
-                await Task.Delay(1000, stoppingToken);
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Internal Error");
+                }
+            }
+        }
+
+        private ComputerWriteViewModel GetStaticInformationOfComputer()
+        {
+            //Create Object
+            ComputerWriteViewModel computerViewModel = new();
+            double MaxRam = 0;
+            double UsedRam = 0;
+
+            //////////////////////////
+            //Get All Information
+            PerformanceCounter pourcentageUseCpu = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total", true);
+            pourcentageUseCpu.NextValue();
+            PerformanceCounter theMemCounter = new PerformanceCounter("Memory", "Available MBytes");
+            theMemCounter.NextValue();
+            PerformanceCounter upTimeSystem = new PerformanceCounter("System", "System Up Time");
+            upTimeSystem.NextValue();
+
+            ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ObjectQuery wql2 = new ObjectQuery("SELECT * FROM Win32_computersystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+            ManagementObjectSearcher searcher2 = new ManagementObjectSearcher(wql2);
+            ManagementObjectCollection results = searcher.Get();
+            ManagementObjectCollection results2 = searcher2.Get();
+
+            //////////////////////////
+            ///Write Computer
+
+            foreach (ManagementObject result in results)
+            {
+                //Update Max Ram
+                computerViewModel.MaxRam = Convert.ToDouble(result["TotalVisibleMemorySize"].ToString()) / 1000000; //Transform to Giga Octet
+                MaxRam = Convert.ToDouble(result["TotalVisibleMemorySize"].ToString());
+                UsedRam = Convert.ToDouble(result["FreePhysicalMemory"].ToString());
+            }
+
+            foreach (ManagementObject result in results2)
+            {
+                //Update Computer Name
+                computerViewModel.Name = result["Name"].ToString();
+
+                //Update Domain
+                computerViewModel.Domain = result["Domain"].ToString();
+
+                //Update NumberOfLogicalProcessors
+                computerViewModel.NumberOfLogicalProcessors = Convert.ToDouble(result["NumberOfLogicalProcessors"].ToString());
+
+                //Update OS
+                string pathOfRegistry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion";
+                computerViewModel.OS = Registry.GetValue(pathOfRegistry, "productName", "Undefined").ToString();
+
+                //Update OSVersion
+                computerViewModel.OSVersion = $"{Environment.OSVersion.Version} ({Registry.GetValue(pathOfRegistry, "displayVersion", "Undefined").ToString()})";
+            }
+
+            //Update Ip
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            if (host.AddressList.Where(x => x.AddressFamily == AddressFamily.InterNetwork).Any())
+            {
+                computerViewModel.Ip = host.AddressList.Where(x => x.AddressFamily == AddressFamily.InterNetwork).First().ToString();
+            }
+
+            //Update UserName
+            computerViewModel.UserName = Environment.UserName;
+
+            //Update SerialNumber
+            ManagementObjectSearcher mbs = new ManagementObjectSearcher("Select * from Win32_BaseBoard");
+            foreach (ManagementObject mo in mbs.Get())
+            {
+                computerViewModel.SerialNumber = mo["SerialNumber"].ToString().Trim();
+            }
+
+            //////////////////////////
+            /// Write Computer Data
+
+            computerViewModel.ComputerLastData = new()
+            {
+                MemoryUsed = UsedRam,
+                ProcessorUtilityPourcent = pourcentageUseCpu.NextValue(),
+                UptimeSinceInSecond = upTimeSystem.NextValue()
+            };
+
+            //////////////////////////
+            /// Write Computer HardDrive
+            foreach (var drive in DriveInfo.GetDrives().Where(item => item.DriveType == DriveType.Fixed))
+            {
+                computerViewModel.ComputerHardDrives.Add(new()
+                {
+                    Letter = drive.Name,
+                    SpaceUse = drive.TotalSize - drive.AvailableFreeSpace,
+                    TotalSpace = drive.TotalSize
+                });
+            }
+
+            return computerViewModel;
+        }
+
+        private async Task<Guid> CheckIfComputerExist(string computerName, string serialNumber)
+        {
+            HttpClient client = new HttpClient();
+            string path = $"{_apiPath}/Computers/{computerName}/{serialNumber}";
+
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string computerId = await response.Content.ReadAsStringAsync();
+                computerId = computerId.Replace("\"", string.Empty);
+                return Guid.Parse(computerId);
+            }
+            else
+            {
+                return Guid.Empty;
+            }
+        }
+
+        private async Task<Guid> CreateComputerInDataBase(ComputerWriteViewModel computerWriteViewModel)
+        {
+            HttpClient client = new HttpClient();
+            string path = $"{_apiPath}/Computers";
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(path, computerWriteViewModel);
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                Device device = await response.Content.ReadFromJsonAsync<Device>();
+                return device.Id;
+            }
+            else
+            {
+                return Guid.Empty;
+            }
+        }
+
+        private async Task<Guid> UpdateComputerInDataBase(ComputerWriteViewModel computerWriteViewModel)
+        {
+            HttpClient client = new HttpClient();
+            string path = $"{_apiPath}/Computers/{computerWriteViewModel.Id}";
+
+            HttpResponseMessage response = await client.PutAsJsonAsync(path, computerWriteViewModel);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                ComputerReadViewModel device = await response.Content.ReadFromJsonAsync<ComputerReadViewModel>();
+                return device.Id;
+            }
+            else
+            {
+                return Guid.Empty;
+            }
+        }
+
+        private async Task AddComputerData(ComputerDataViewModel computerDataViewModel)
+        {
+            HttpClient client = new HttpClient();
+            string path = $"{_apiPath}/ComputersData";
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(path, computerDataViewModel);
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                _logger.LogInformation($"Computer data added");
+            }
+        }
+
+        private async Task AddComputerHardDrive(ComputerHardDriveViewModel computerHardDriveViewModel)
+        {
+            HttpClient client = new HttpClient();
+            string path = $"{_apiPath}/ComputersHardDrive/{computerHardDriveViewModel.ComputerId}";
+
+            HttpResponseMessage response = await client.PutAsJsonAsync(path, computerHardDriveViewModel);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                _logger.LogInformation($"Computer HardData added");
             }
         }
     }
